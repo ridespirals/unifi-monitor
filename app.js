84594f540dfa596cf75ced687d119ef8f0b2e178
John Varga <john.varga@unifilabs.com>
2017-03-06 15:33:10 -0800

    minor electron startup changes from electron-quickstart

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 import { app, BrowserWindow } from 'electron'
 import url from 'url'
 
-let window = null
-app.on('ready', () => {
-    window = new BrowserWindow({
+let mainWindow = null
+
+function createWindow() {
+
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         backgroundColor: '#f7f7f7',
-        show: false
+        show: true
     })
 
-    window.loadURL(url.format({
+    mainWindow.loadURL(url.format({
         pathname: `${__dirname}/index.html`,
         protocol: 'file:',
         slashes: true
     }))
 
-    window.once('ready-to-show', () => {
-        window.show()
+    mainWindow.on('closed', () => {
+        mainWindow = null
     })
 
-    window.on('closed', () => {
-        window = null
-    })
+}
+
+app.on('ready', createWindow)
+
+// quit when all windows are closed
+app.on('window-all-closed', () => {
+    // on OSX it is common for apps and their menu bar
+    // to stay active until the user quits explicitly
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // on OSX it's common to re-create a window in the app
+    // when the dock icon is clicked and there are not other
+    // windows open
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
