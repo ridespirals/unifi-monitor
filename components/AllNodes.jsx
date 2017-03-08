b7d6333ead7d67e8fb937455003a0037ba2ba18c
John Varga <john.g.varga@gmail.com>
2017-03-07 23:18:13 -0800

    nodes component

--- components/AllNodes.jsx
+++ components/AllNodes.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import _ from 'lodash'
+import bytes from 'bytes'
 
 const mapStateToProps = (state) => {
     return {
@@ -18,16 +19,33 @@ class AllNodes extends Component {
     }
     render() {
         let { nodes } = this.props
-        console.debug('<all nodes>', this.props)
         return (
-            <ul className="all-nodes-list">
+            <div className="all-nodes-list">
                 {nodes.map((node, idx) => {
-                    console.debug('~map node~', node)
+                    let diskUsage = node.monitorData['hudson.node_monitors.DiskSpaceMonitor']
+                    let diskSize = bytes(diskUsage ? diskUsage.size : 0)
+                    console.debug("-- node --", node)
+                    let idleClass = node.idle
+                        ? 'hourglass-o'
+                        : 'cog fa-spin'
+                    idleClass = `fa fa-${idleClass}`
                     return (
-                        <li className="node-item" key={idx}>{node.displayName}</li>
+                        <div className="node-item" key={idx}>
+                            <h3>{node.displayName}</h3>
+                            <table className="table table-inverse table-sm">
+                                <thead className="thead-inverse"><tr>
+                                    <th>Disk</th>
+                                    <th>Idle</th>
+                                </tr></thead>
+                                <tbody><tr>
+                                    <td>{diskSize}</td>
+                                    <td><i className={idleClass} /></td>
+                                </tr></tbody>
+                            </table>
+                        </div>
                     )
                 })}
-            </ul>
+            </div>
         )
     }
 }
